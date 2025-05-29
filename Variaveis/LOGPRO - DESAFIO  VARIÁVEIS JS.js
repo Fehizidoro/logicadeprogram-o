f7ef68e89aa80280472d4ff453baa84b14e6a812
feizidoro <feizidoro24@gmail.com>
2025-05-29 11:18:06 -0300

    variaveis

---
+++ Variaveis/LOGPRO - DESAFIO  VARIÁVEIS JS.js
@@ -0,0 +1,19 @@
+/*let  nome = prompt("Digite seu Nome: ")
+let idade = Number(prompt("Digite sua Idade: "))
+let email = prompt("Digite seu Email: ")
+let numero = Number(prompt("Digite seu Numero: "))
+console.log("O seu nome é", nome, "você tem",idade, "anos, o seu email é", email, "e o seu numero de telefone é", numero, "Correto seja Bem Vindo ao nosso site")*/
+
+
+let anoDeNascimento = Number(prompt("Digite o seu ano de nascimento:" ))
+let subtração = 2025 - anoDeNascimento
+console.log ("A sua idade é:", subtração)
+
+
+
+let produto = "Camiseta"
+let preco = 49.90
+let estoque = 150
+console.log (produto)
+console.log (preco + preco)
+console.log (estoque+estoque)
---
+++ Variaveis/LOGPRO Atividade 1 - Variaveis.js
@@ -0,0 +1,35 @@
+/*let Nome="fernanda "
+let Nome2="oliveira"
+console.log (Nome+Nome2)/*
+
+/*let idade = 16
+console.log (idade)*/
+
+/*let aprendendoJavaScript = "true"
+console.log (aprendendoJavaScript)*/
+
+/*let nome2 = "Fernanda"
+let sobrenome = "Oliveira"
+console.log (nome2+sobrenome)*/
+
+/*let numero1 = "24"
+let numero2 = "8"
+console.log (numero1+numero2)*/
+
+/*let numero1 = "24"
+let numero2 = "28"
+console.log (numero1*numero2)*/
+
+/*let cidade = "ferraz "
+let habitantes = "194,276"
+console.log (cidade+habitantes)*/
+
+/*let preco = 50
+preco+=5
+console.log (preco)*/
+
+/*let meuvalor = 0 
+console.log (meuvalor*/
+
+/*let meunulo = null
+console.log (meuvalor)*/
